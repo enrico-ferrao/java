67ca29934f5e1959d7afe598ac38f91b5914fe69
enrico-ferrao <enricogferrao@gmail.com>
2026-03-05 20:28:48 -0300

    Update Personagem.java

--- sobrevivente/src/main/java/br/maua/Personagem.java
+++ sobrevivente/src/main/java/br/maua/Personagem.java
@@ -8,51 +8,72 @@ public class Personagem {
     
     //método: tipo de retorno, nome, lista de parâmetros e corpo
     
+
    
+
     void cacar(){
-         if (energia < 1 || fome == 10){
-            vida--;
+        if(vida==0){
+            System.out.println("voce morreu!");
         }
-        if(energia >= 2){
-            System.out.println(nome + " caçando...");
-            energia = energia - 2;
-        }else{
-            System.out.printf("%s sem energia para caçar...\n", nome);
+        else {
+
+            if (energia < 1 || fome == 10){
+                vida--;
+            }
+            if(energia >= 2){
+                System.out.println(nome + " caçando...");
+                energia = energia - 2;
+            }else{
+                System.out.printf("%s sem energia para caçar...\n", nome);
+            }
+            if(fome < 10)
+                fome = fome + 1;
+            if(sono < 10)
+                sono = sono + 1;
         }
-        if(fome < 10)
-            fome = fome + 1;
-        if(sono < 10)
-            sono = sono + 1;
     }
 
     void comer(){
-         if (energia < 1 || fome == 10){
-            vida--;
+        if(vida==0){
+            System.out.println("voce morreu!");
         }
-        if(fome >= 1){
-            System.out.printf("%s comendo...\n", nome);
-            // fome = fome - 1;
-            // fome -= 1;
-            fome--;
-            if(energia < 10)
-                energia++;
-        }
-        else{
-            System.out.printf("%s sem fome\n", nome);   
+        else {
+            if (energia < 1 || fome == 10){
+                vida--;
+            }
+            if(fome >= 1){
+                System.out.printf("%s comendo...\n", nome);
+                // fome = fome - 1;
+                // fome -= 1;
+                fome--;
+                if(energia < 10)
+                    energia++;
+                if(vida < 10)
+                    vida++;
+            }
+            else{
+                System.out.printf("%s sem fome\n", nome);   
+            }
         }
     }
 
+
     void dormir(){
-         if (energia < 1 || fome == 10){
-            vida--;
+        if(vida==0){
+                System.out.println("voce morreu!");
         }
-        if(sono >= 1){
-            System.out.printf("%s dormindo...\n", nome);
-            --sono;
-            energia = Math.min(energia + 1, 10);
-        }
-        else{
-            System.out.printf("%s sem sono\n", nome);
+        else {
+            if (energia < 1 || fome == 10){
+                vida--;
+            }
+            if(sono >= 1){
+                System.out.printf("%s dormindo...\n", nome);
+                --sono;
+                energia = Math.min(energia + 1, 10);
+            }
+            else{
+                System.out.printf("%s sem sono\n", nome);
+            }
         }
     }
     
