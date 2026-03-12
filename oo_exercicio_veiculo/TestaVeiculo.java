d0b172de5e975af9f0bf2a56ab6ae16d091c0995
enrico-ferrao <enricogferrao@gmail.com>
2026-03-11 21:39:51 -0300

    Create TestaVeiculo.java

---
+++ oo_exercicio_veiculo/TestaVeiculo.java
@@ -0,0 +1,8 @@
+public class TestaVeiculo {
+    public static void main(String[] args) {
+        Veiculo v1 = new Veiculo();
+        v1.nome = "Serjão";
+        v1.correr();
+        v1.concertar();
+    }
+}
