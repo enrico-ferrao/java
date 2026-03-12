b16c042db1a1b8815356d5db65e153ef9cc40ba0
enrico-ferrao <enricogferrao@gmail.com>
2026-03-12 19:38:52 -0300

    Delete TestaVeiculo.java

--- oo_exercicio_veiculo/TestaVeiculo.java
+++
@@ -1,8 +0,0 @@
-public class TestaVeiculo {
-    public static void main(String[] args) {
-        Veiculo v1 = new Veiculo();
-        v1.nome = "Serjão";
-        v1.correr();
-        v1.concertar();
-    }
-}
