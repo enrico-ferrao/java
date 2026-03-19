abbaf4eca3a520370ddc1a1f61717c0adc968b8a
enrico-ferrao <enricogferrao@gmail.com>
2026-03-18 21:11:17 -0300

    Update Teste.java

--- Banco/Teste.java
+++ Banco/Teste.java
@@ -1,12 +1,34 @@
+import java.util.Random;
+
 public class Teste {
-    public static void main(String[] args) {
+
+    public static void main(String[] args)  throws Exception {
+        Random gerador = new Random();
         ContaBancaria c1 = new ContaBancaria("Enrico", "1001", 0);
-        c1.depositar (50);
-        c1.exibirExtrato();
         ContaBancaria c2 = new ContaBancaria ("Gabriel", "1002",0);
-        c2.depositar(100);
-        c2.transferir(c1, 100);
-        c1.exibirExtrato();
-        c2.exibirExtrato();
+        while (true) {
+            int oQueFazer = gerador.nextInt(1, 6);
+            switch(oQueFazer){
+                case 1:
+                    c1.depositar(50);
+                    break;
+                case 2:
+                    c1.sacar(50);
+                    break;
+                case 3:
+                    c1.transferir(c2, 50);
+                    break;
+                case 4:
+                    c1.consultarSaldo();
+                    break;
+                case 5:
+                    c1.exibirExtrato();
+            }
+            System.out.println("======================");
+            Thread.sleep(8000);
+        }
+       
     }
+    
 }
+
