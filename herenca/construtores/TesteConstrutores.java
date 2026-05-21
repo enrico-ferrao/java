de7660377a412a9b615d806fe6a2c90cfa650df8
Enrico Ferrao <enricogferrao@gmail.com>
2026-05-21 19:53:35 -0300

    Create TesteConstrutores.java

---
+++ herenca/construtores/TesteConstrutores.java
@@ -0,0 +1,7 @@
+public class TesteConstrutores {
+    public static void main(String[] args) {
+        Pessoa p = new Pessoa("Ana", 20);
+        Aluno a = new Aluno("Joao", 19);
+        System.out.println(p);
+    }
+}
