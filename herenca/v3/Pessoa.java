8024790f3ad88da5d01ae6028e2eb7e8618f5ecb
Enrico Ferrao <enricogferrao@gmail.com>
2026-05-21 19:54:23 -0300

    Create Pessoa.java

---
+++ herenca/v3/Pessoa.java
@@ -0,0 +1,10 @@
+public class Pessoa {
+    String nome;
+    public String public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    int idade;
+} 
