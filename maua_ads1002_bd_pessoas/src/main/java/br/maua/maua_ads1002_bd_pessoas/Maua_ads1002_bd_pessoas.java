63213d9e7317ecf0f4efb878edf3ea096ebb3a2b
Enrico Ferrao <enricogferrao@gmail.com>
2026-05-21 19:53:40 -0300

    Create Maua_ads1002_bd_pessoas.java

---
+++ maua_ads1002_bd_pessoas/src/main/java/br/maua/maua_ads1002_bd_pessoas/Maua_ads1002_bd_pessoas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.maua.maua_ads1002_bd_pessoas;
+
+/**
+ *
+ * @author rodrigo
+ */
+public class Maua_ads1002_bd_pessoas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
